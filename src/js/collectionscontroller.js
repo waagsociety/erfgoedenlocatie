be08a170abe1dd6b62c2614177cce658ec190240
Taco van Dijk <taco@Orbo>
2014-07-15 14:26:14 +0200

    openlayers <-> angular

--- src/js/collectionscontroller.js
+++ src/js/collectionscontroller.js
@@ -5,8 +5,7 @@
 
 function CollectionsController($scope)
 {
-
-      //dummy data
+          //dummy data
       $scope.defaultCollection = [
       { "type": "Feature", "properties": { "id": 8, "Titel": "De Hoop", "LONG": 3.0, "LAT": 51.0, "PHOTO": "http:\/\/images.memorix.nl\/rce\/thumb\/400x400\/efdf0540-8c17-82e7-0b23-e80ac04991b9.jpg" }, "geometry": { "type": "Point", "coordinates": [ 3.913555902187822, 51.652908190029599 ] } },
       { "type": "Feature", "properties": { "id": 7, "Titel": "De Drie Waaien", "LONG": 3.0, "LAT": 51.0, "PHOTO": "http:\/\/images.memorix.nl\/rce\/thumb\/400x400\/57f1e541-a240-8d15-ffb4-74f70aecdcaa.jpg" }, "geometry": { "type": "Point", "coordinates": [ 3.662566033391347, 51.543412097264124 ] } },
@@ -17,5 +16,7 @@ function CollectionsController($scope)
       { "type": "Feature", "properties": { "id": 2, "Titel": "Onderslagmolen", "LONG": 3.0, "LAT": 51.0, "PHOTO": "http:\/\/images.memorix.nl\/rce\/thumb\/400x400\/5d34290c-fcba-a85d-813a-d77b80ba0267.jpg" }, "geometry": { "type": "Point", "coordinates": [ 3.743085904594841, 51.697442043750883 ] } },
       { "type": "Feature", "properties": { "id": 1, "Titel": "Molenrestant", "LONG": 3.0, "LAT": 51.0, "PHOTO": "http:\/\/images.memorix.nl\/rce\/thumb\/400x400\/c15935bf-5fcb-21be-ed67-e373ad49133a.jpg" }, "geometry": { "type": "Point", "coordinates": [ 3.890342966345374, 51.502339427218807 ] } },
       { "type": "Feature", "properties": { "id": 0, "Titel": "De Achtkante Molen", "LONG": 3.0, "LAT": 51.0, "PHOTO": "http:\/\/images.memorix.nl\/rce\/thumb\/800x800\/45fd1cc3-cd0b-bb79-66a0-e017f19af4db.jpg" }, "geometry": { "type": "Point", "coordinates": [ 3.611062331990914, 51.49375913957671 ] } }
-     ]; 
+     ];
+
+     updateMarkers($scope.defaultCollection); 
 }
--- src/js/ol_marker.js
+++ src/js/ol_marker.js
@@ -1,31 +1,36 @@
-var raster = new ol.layer.Tile({
-      source: new ol.source.OSM()
-    });
-    
-var format = new ol.format.WKT();
-var feature = format.readFeature(
-    'POINT(5.65823792189898622 53.21705934740188582)');
-feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
+function updateMarkers(collection)
+{
+  console.log(collection);
 
-var iconStyle = new ol.style.Style({
-  image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-    src: 'icons/windmill-2_1.png'
-  }))
-});
+  var raster = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
 
-feature.setStyle(iconStyle);
-	
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: [feature]
-  })
-});
+  var format = new ol.format.WKT();
+  var feature = format.readFeature(
+      'POINT(5.65823792189898622 53.21705934740188582)');
+  feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
 
-var map = new ol.Map({
-  layers: [raster, vector],
-  target: 'mapPanel',
-  view: new ol.View({
-    center: [640696, 7002528],
-    zoom: 9
-  })
-});
+  var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+      src: 'icons/windmill-2_1.png'
+    }))
+  });
+
+  feature.setStyle(iconStyle);
+
+  var vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: [feature]
+    })
+  });
+
+  var map = new ol.Map({
+    layers: [raster, vector],
+      target: 'mapPanel',
+      view: new ol.View({
+        center: [640696, 7002528],
+      zoom: 9
+      })
+  });
+}
