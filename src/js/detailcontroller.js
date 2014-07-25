9dbb0c74510b2fd7f098197d47106fd3481dd049
Taco van Dijk <taco@Orbo>
2014-07-25 16:21:06 +0200

    redirection on refresh

--- src/js/detailcontroller.js
+++ src/js/detailcontroller.js
@@ -7,6 +7,16 @@ angular.module('elviewer').controller('DetailController', ['$scope', '$routePara
     var item = Repository.defaultCollection.filter(function(item){
             return item.uid === itemId;
         })[0];
+  
+    //we are here on a refresh 
+    if(item == undefined)
+    { 
+
+        //TODO: make a call to virtuoso for this specific unique ID
+
+        //redirect
+        location.href = "#/";
+    }
 
     //get the index in spatial filter
     var index = Repository.spatialSelection.indexOf(item);
@@ -14,6 +24,7 @@ angular.module('elviewer').controller('DetailController', ['$scope', '$routePara
     //setup variables for databinding
     $scope.Repository = Repository;
     $scope.itemIndex = index + 1;
+    $scope.itemId = itemId;
     $scope.item = item; 
 
     //return the uid of the next item
@@ -29,7 +40,7 @@ angular.module('elviewer').controller('DetailController', ['$scope', '$routePara
         }
         else
         {
-            return $scope.item.uid;
+            return $scope.itemId;
         }
     };
 
@@ -40,7 +51,7 @@ angular.module('elviewer').controller('DetailController', ['$scope', '$routePara
         var previous = Repository.spatialSelection[index - 1];
         if(previous == undefined)
         {
-            return $scope.item.uid;
+            return $scope.itemId;
         }
         return previous.uid;
     };
